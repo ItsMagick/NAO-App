e4a914c8cda780831541fea4291b45169bd5ca72
Sebastian Peschke <magick930@googlemail.com>
2022-05-03 10:47:00 +0200

    Initial Commit

---
+++ Shared/NAO_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAO_AppApp.swift
+//  Shared
+//
+//  Created by Sebastian Peschke on 03.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NAO_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
