f8390d4d78cc571f5028ee85bbb2438375f1b49c
Jan <jan.rubner@hof-university.de>
2022-05-06 13:08:07 +0200

    SCHMUTZ
    VStack Alignment ist Schmutz, Burak pls fix

--- Shared/Main/MainView.swift
+++ Shared/Main/MainView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        Text("Main")
-    }
+    
+//dummy data
+  @State var disciplines = ["Motion", "Speech Text", "Recorder", ".", ".","."]
+
+  var body: some View {
+//list for mainView w/ dummy data in VStack
+      VStack{
+          //placeholder for camera
+          Rectangle()
+              .size(width: UIScreen.main.bounds.width, height: (UIScreen.main.bounds.height)/3)
+              .padding()
+              
+
+          List(disciplines, id: \.self) { discipline in
+            Text(discipline)
+                  .font(.headline)
+          }
+      }
+  }
 }
+
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
--- Shared/NAO_AppApp.swift
+++ Shared/NAO_AppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NAO_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            FindNaoView()
+            MainView()
         }
     }
 }
