7a14b824e5481d59fdf83eb1d92a8a442d7e7c78
Burak Güccük <burak58241@web.de>
2022-05-13 12:33:11 +0200

    Nao List

--- Shared/FindNao/FindNaoView.swift
+++ Shared/FindNao/FindNaoView.swift
@@ -12,32 +12,23 @@ struct FindNaoView: View {
     var naoData = ["Nao", "Nao2", "Nao3", "Nao4", "Nao5", "Nao6"]
     
     var body: some View {
-        VStack{
-                
-            NavigationView{
-                List(naoData, id: \.self){ nao in
-                    Text(nao)
-                    
-                }.navigationTitle("NAO")
-            }
         
-            HStack{
-                Button("Refresh") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
+            NavigationView{
                 
-                Button("Enter Custom IP") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }
-                
+                List(naoData, id: \.self) { nao in
+                    Text(nao)
+                }.navigationTitle("Nao Liste")
+                    
             }
+            
+        
         }
         
             
             
     
-    }
 }
+
 
 struct FindNaoView_Previews: PreviewProvider {
     static var previews: some View {
